87063c411420be27eafb092afa39dd834ada25a9
George Dzhgarkava <dg@roommediagroup.com>
2013-08-14 15:17:13 +0400

    Add: Database migration tool (blank)

--- TierSponsors_Service/TierSponsorsService.svc.cs
+++ TierSponsors_Service/TierSponsorsService.svc.cs
@@ -12,24 +12,6 @@ namespace TierSponsors_Service
     // NOTE: In order to launch WCF Test Client for testing this service, please select TierSponsorsService.svc or TierSponsorsService.svc.cs at the Solution Explorer and start debugging.
     public class TierSponsorsService : ITierSponsorsService
     {
-        public string GetData(int value)
-        {
-            return string.Format("You entered: {0}", value);
-        }
-
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
-        {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
-        }
-
         private OrganisationsCollection SearchOrganisations(string searchQuery)
         {
             var organisations = new OrganisationsCollection
---
+++ Tools/TierSponsors_MigrateDB/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace TierSponsors_MigrateDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var connectionString = ConfigurationManager.ConnectionStrings["DB_ConnectionString"].ConnectionString;
+            var connectionString = "Data Source=sql07-8-eng.corp.parking.ru;Initial Catalog=yourtown_20;User ID=yourtown_20;Password=[6[b!Lpc-^";
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var da = new SqlDataAdapter("select * from yourtown_20.dbo.Organisation", connectionString);
+                var ds = new DataSet();
+                da.Fill(ds);
+
+            }
+        }
+    }
+}
