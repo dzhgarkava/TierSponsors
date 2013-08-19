f8b117f6272979ffec65a4783e09264cf075c163
George Dzhgarkava <dg@roommediagroup.com>
2013-08-19 18:03:18 +0400

    Add: silverlight database editor

---
+++ Tools/TierSponsors_EditDB.Web/ServiceEditDB_SL.svc.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+
+namespace TierSponsors_EditDB.Web
+{
+
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class ServiceEditDB_SL
+    {
+        const string sDataBaseName = "TierSponsors_DB";
+        //const string sDataBaseName = "yourtown_20"; // Use it for parking database
+        readonly string _connectionString = ConfigurationManager.ConnectionStrings["TS_ConnectionString"].ConnectionString;
+
+        [OperationContract]
+        public OrganisationsCollection GetOrganisations(bool Checked)
+        {
+            var organisations = new OrganisationsCollection
+            {
+                OrgList = new List<Organisation>()
+            };
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var da = new SqlDataAdapter("select * from " + sDataBaseName + ".dbo.Organisations", _connectionString);
+                var ds = new DataSet();
+                da.Fill(ds);
+
+                var searchQuery = Checked ? "Checked LIKE 'OK'" : "Checked LIKE 'FALSE'";
+
+                DataRow[] dr = ds.Tables[0].Select(searchQuery);
+                for (int i = 0; i < dr.Count(); i++)
+                {
+                    var org = new Organisation
+                    {
+                        ID = dr[i]["ID"].ToString(),
+                        Name = dr[i]["Name"].ToString(),
+                        City = dr[i]["City"].ToString(),
+                        County = dr[i]["County"].ToString(),
+                    };
+                    organisations.OrgList.Add(org);
+                }
+            }
+            return organisations;
+        }
+
+        [OperationContract]
+        public Organisation GetOrganisationByID(string ID)
+        {
+            Organisation org;
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var da = new SqlDataAdapter("select * from " + sDataBaseName + ".dbo.Organisations", _connectionString);
+                var ds = new DataSet();
+                da.Fill(ds);
+
+                var searchQuery = "ID = " + ID;
+
+                DataRow[] dr = ds.Tables[0].Select(searchQuery);
+
+                org = dr.Any()
+                    ? new Organisation
+                    {
+                        ID = dr[0]["ID"].ToString(),
+                        Name = dr[0]["Name"].ToString(),
+                        City = dr[0]["City"].ToString(),
+                        County = dr[0]["County"].ToString(),
+                    }
+                    : null;
+            }
+            return org;
+        }
+
+        // Add more operations here and mark them with [OperationContract]
+        public class OrganisationsCollection
+        {
+            public int TotalOrgs { get { return OrgList.Count; } }
+            public List<Organisation> OrgList { get; set; }
+        }
+
+        public class Organisation
+        {
+            public string ID { get; set; }
+            public string Name { get; set; }
+            public string City { get; set; }
+            public string County { get; set; }
+            public string NameCityCounty { get; set; }
+        }
+    }
+}
---
+++ Tools/TierSponsors_EditDB/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using TierSponsors_EditDB.srEditDB_SL;
+
+
+namespace TierSponsors_EditDB
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ServiceEditDB_SLClient client = new ServiceEditDB_SLClient();
+            client.GetOrganisationsCompleted += client_GetOrganisationsCompleted;
+            client.GetOrganisationsAsync(true);
+        }
+
+        void client_GetOrganisationsCompleted(object sender, GetOrganisationsCompletedEventArgs e)
+        {
+            if (e.Result != null)
+            {
+                foreach (var org in e.Result.OrgList)
+                {
+                    ListBoxItem item = new ListBoxItem();
+                    item.MouseLeftButtonDown += item_MouseLeftButtonDown;
+                    lbxOrganisations.Items.Add(org.ID);
+                }
+            }
+        }
+
+        void item_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ServiceEditDB_SLClient client = new ServiceEditDB_SLClient();
+            client.GetOrganisationByIDCompleted += client_GetOrganisationByIDCompleted;
+            client.GetOrganisationByIDAsync(sender.ToString());
+        }
+
+        void client_GetOrganisationByIDCompleted(object sender, GetOrganisationByIDCompletedEventArgs e)
+        {
+            txtID.Text = e.Result.ID;
+            txtNameCityCounty.Text = e.Result.NameCityCounty;
+            txtCity.Text = e.Result.City;
+            txtCounty.Text = e.Result.County;
+        }
+
+        private void txtID_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+           // btnSave.IsEnabled = !String.IsNullOrEmpty(txtID.Text);
+        }
+    }
+}
