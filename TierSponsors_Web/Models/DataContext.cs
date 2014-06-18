74b86a5416ec26262ef168052a5b2e7bdf9f0229
George Dzhgarkava <dg@roommediagroup.com>
2014-06-18 12:10:40 +0400

    Added: New WebSite project

---
+++ TierSponsors_Web/Models/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TierSponsors_Web.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("database")
+        {
+        }
+
+        public DbSet<Organisation> Organisations { get; set; }
+    }
+}
---
+++ TierSponsors_Web/Models/Organisation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TierSponsors_Web.Models
+{
+    public class Organisation
+    {
+        public int Id { get; set; }
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+    }
+}
