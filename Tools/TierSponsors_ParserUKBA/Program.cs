387a947a56a5a636fae3e7bca48f375b9fcb5085
George Dzhgarkava <dg@roommediagroup.com>
2014-07-24 23:14:49 +0400

    Significant changes

--- Tools/TierSponsors_ParserUKBA/Program.cs
+++ Tools/TierSponsors_ParserUKBA/Program.cs
@@ -1,102 +1,166 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
+using System.Linq.Expressions;
+using System.Threading;
 
 
 namespace TierSponsors_ParserUKBA
 {
     class Program
     {
-        private const string PathToFile = "pbsregisterofsponsors_tier2.pdf";
+        private const string PathToFile = "sponsorslist.txt";
         static void Main()
         {
-            // Перепивать на работу с текстовым файлом
-            //var pdfDocument4 = new PDFDocument4Class();
-            //pdfDocument4.Open(PathToFile, "");
-            //int pagecount = pdfDocument4.GetPageCount();
-
             var block = new List<String>();
 
-            //for (int i = 0; i < pagecount; i++)
+            TextReader reader = File.OpenText(PathToFile);
+            string line = "";
+            var count = 0;
+            var isStarted = false;
+            
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (isStarted)
+                {
+                    if (line.IndexOf("Organisation Name Town/City County Tier & Rating Sub Tier ", StringComparison.Ordinal) == -1)
+                        if (!String.IsNullOrEmpty(line))
+                            if (!(line.Contains("Page ") && line.Contains(" of ")))
+                                if (line != "\f")
+                                {
+                                    if (line[line.Length - 1] == ' ')
+                                        line = line.Remove(line.Length - 1);
+                                    block.Add(line);
+                                }
+                }
+                else
+                {
+                    if (line.IndexOf("Organisation Name Town/City County Tier & Rating Sub Tier ", StringComparison.Ordinal) == 0)
+                    {
+                        isStarted = true;
+                    }
+                }
+                count++;
+            }
+
+            for (int i = 0; i < 4; i++)
+            block.RemoveAt(block.Count - 1);
+
+            // Определяем различные типы виз и их разновидности
+            //var tiers = new List<String>();
+            //var subTiers = new List<String>();
+
+            //while (block.Count > 0)
             //{
-            //    string page = pdfDocument4.GetPageText(i);
-            //    int pos;
-            //    while ((pos = page.IndexOf("\r\n", StringComparison.Ordinal)) != -1)
+            //    if (block[0].IndexOf(" rating)", StringComparison.Ordinal) == -1)
             //    {
-            //        block.Add(page.Remove(pos));
-            //        page = page.Remove(0, pos + 2);
+            //        if (block.Count == 0) break;
+
+            //        var org = new Organisation {Name = block[0]};
+            //        //Console.WriteLine(block[0].ToString());
+            //        block.RemoveAt(0);
+
+            //        if (block.Count == 0) break;
+            //        while (block[0].IndexOf(" rating)", StringComparison.Ordinal) != -1)
+            //        {
+            //            //Console.WriteLine(block[0].ToString());
+            //            var tierandrating = block[0];
+            //            org.TierAndRating += tierandrating.Remove(tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 8) + ";";
+            //            org.SubTier += tierandrating.Remove(0, tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 9) + ";";
+
+            //            // проверяем разные Тиер
+            //            var addtier = true;
+            //            foreach (string t1 in tiers)
+            //            {
+            //                if (t1 == tierandrating.Remove(tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 8)) addtier = false;
+            //            }
+            //            if (addtier) tiers.Add(tierandrating.Remove(tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 8));
+
+            //            if (tierandrating.Remove(tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 8).IndexOf("Tier 5", StringComparison.Ordinal) != -1)
+            //            {
+            //                var addsubtier = true;
+            //                foreach (string s1 in subTiers)
+            //                {
+            //                    if (s1 == tierandrating.Remove(0, tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 9)) addsubtier = false;
+            //                }
+            //                if (addsubtier) subTiers.Add(tierandrating.Remove(0, tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 9));
+            //            }
+
+            //            block.RemoveAt(0);
+            //            if (block.Count == 0) break;
+            //        }
             //    }
+            //    Console.WriteLine();
             //}
 
-            while (block[0].IndexOf("Organisation Name Town/City County Tier & Rating Sub Tier", StringComparison.Ordinal) == -1)
-            {
-                block.RemoveAt(0);
-            }
-            block.RemoveAt(0);
+            var orgs = new List<Organisation>();
 
             for (int i = 0; i < block.Count; i++)
             {
-                if (block[i].IndexOf("Organisation Name Town/City County Tier & Rating Sub Tier", StringComparison.Ordinal) != -1)
+                var org = new Organisation {Name = block[i]};
+
+                while (true)
                 {
-                    block.RemoveAt(i);
-                    i--;
-                }
-            }
+                    if (i + 1 == block.Count) break;
 
-            // /*
-            // Определяем различные типы виз и их разновидности
-            var tiers = new List<String>();
-            var subTiers = new List<String>();
-
-            while (block.Count > 0)
-            {
-                if (block[0].IndexOf(" rating)", StringComparison.Ordinal) == -1)
-                {
-                    if (block.Count == 0) break;
-
-                    var org = new Organisation {Name = block[0]};
-                    //Console.WriteLine(block[0].ToString());
-                    block.RemoveAt(0);
-
-                    if (block.Count == 0) break;
-                    while (block[0].IndexOf(" rating)", StringComparison.Ordinal) != -1)
+                    var visainfo = block[i + 1];
+                    if (visainfo.Contains(" (Premium))"))
                     {
-                        //Console.WriteLine(block[0].ToString());
-                        var tierandrating = block[0];
-                        org.TierAndRating += tierandrating.Remove(tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 8) + ";";
-                        org.SubTier += tierandrating.Remove(0, tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 9) + ";";
-
-                        // проверяем разные Тиер
-                        var addtier = true;
-                        foreach (string t1 in tiers)
+                        var visa = new Visa
                         {
-                            if (t1 == tierandrating.Remove(tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 8)) addtier = false;
-                        }
-                        if (addtier) tiers.Add(tierandrating.Remove(tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 8));
-
-                        if (tierandrating.Remove(tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 8).IndexOf("Tier 5", StringComparison.Ordinal) != -1)
-                        {
-                            var addsubtier = true;
-                            foreach (string s1 in subTiers)
-                            {
-                                if (s1 == tierandrating.Remove(0, tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 9)) addsubtier = false;
-                            }
-                            if (addsubtier) subTiers.Add(tierandrating.Remove(0, tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 9));
-                        }
-
-                        block.RemoveAt(0);
-                        if (block.Count == 0) break;
+                            TypeAndRating = visainfo.Remove(visainfo.IndexOf(" (Premium))", StringComparison.Ordinal) + 11),
+                            SubType = visainfo.Remove(0, visainfo.IndexOf(" (Premium))", StringComparison.Ordinal) + 12)
+                        };
+                        org.Visas.Add(visa);
+                        i++;
                     }
-
-
+                    else if (visainfo.Contains(" rating)"))
+                    {
+                        var visa = new Visa
+                        {
+                            TypeAndRating = visainfo.Remove(visainfo.IndexOf(" rating)", StringComparison.Ordinal) + 8),
+                            SubType = visainfo.Remove(0, visainfo.IndexOf(" rating)", StringComparison.Ordinal) + 9)
+                        };
+                        org.Visas.Add(visa);
+                        i++;
+                    }
+                    else break;
                 }
-                Console.WriteLine();
+
+                orgs.Add(org);
             }
-            //*/
 
             // Пищем в базу
+            //ModeDataToDb(orgs);
+            
+            Console.WriteLine();
+            Console.ReadKey();
+        }
 
+        public class Organisation
+        {
+            public String Name { get; set; }
+            public String TierAndRating { get; set; }
+            public String SubTier { get; set; }
+            public List<Visa> Visas { get; set; }
+
+            public Organisation()
+            {
+                Visas = new List<Visa>();
+            }
+        }
+
+        public class Visa
+        {
+            public string TypeAndRating { get; set; }
+            public string SubType { get; set; }
+        }
+
+        private static void ModeDataToDb(IEnumerable<Organisation> orgs)
+        {
             const string connectionString = "Data Source=project10-devel;Initial Catalog=test_ilawiuk;Integrated Security=True;User ID=pmo_reporting;Password=Ps71_iE";
             using (var connection = new SqlConnection(connectionString))
             {
@@ -105,40 +169,15 @@ namespace TierSponsors_ParserUKBA
                 var da = new SqlDataAdapter("select * from test_ilawiuk.dbo.tdMain", connectionString);
                 var ds = new DataSet();
                 da.Fill(ds);
-                
-                while (block.Count > 0)
+
+                foreach (var org in orgs)
                 {
-                    if (block[0].IndexOf(" rating)", StringComparison.Ordinal) == -1)
-                    {
-                        if (block.Count == 0) break;
-
-                        var org = new Organisation {Name = block[0]};
-                        //Console.WriteLine(block[0].ToString());
-                        block.RemoveAt(0);
-
-                        if (block.Count == 0) break;
-                        while (block[0].IndexOf(" rating)", StringComparison.Ordinal) != -1)
-                        {
-                            //Console.WriteLine(block[0].ToString());
-                            var tierandrating = block[0];
-                            org.TierAndRating += tierandrating.Remove(tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 8) + ";";
-                            org.SubTier += tierandrating.Remove(0, tierandrating.IndexOf(" rating)", StringComparison.Ordinal) + 9) + ";";
-
-                            block.RemoveAt(0);
-                            if (block.Count == 0) break;
-                        }
-
-                        Console.WriteLine(org.Name);
-
-                        DataRow dr = ds.Tables[0].NewRow();
-                        dr["NameCityCounty"] = org.Name;
-                        dr["TierAndRating"] = org.TierAndRating;
-                        dr["SubTier"] = org.SubTier;
-                        dr["OriginalUKBA"] = org.Name;
-                        ds.Tables[0].Rows.Add(dr);
-
-                    }
-                    //Console.WriteLine();
+                    DataRow dr = ds.Tables[0].NewRow();
+                    dr["NameCityCounty"] = org.Name;
+                    dr["TierAndRating"] = org.TierAndRating;
+                    dr["SubTier"] = org.SubTier;
+                    dr["OriginalUKBA"] = org.Name;
+                    ds.Tables[0].Rows.Add(dr);
                 }
 
                 var builder = new SqlCommandBuilder(da);
@@ -146,16 +185,6 @@ namespace TierSponsors_ParserUKBA
                 da.Update(ds);
                 connection.Close();
             }
-
-            Console.WriteLine();
-            Console.ReadKey();
-        }
-
-        private class Organisation
-        {
-            public String Name { get; set; }
-            public String TierAndRating { get; set; }
-            public String SubTier { get; set; }
         }
     }
 }
